578bb13bc6f919b035870cd9865cb0cdae13dc40
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2025-06-18 12:13:00 +0300

    update dir

--- rgb_lib.go
+++ rgb_lib.go
@@ -1,7 +1,7 @@
 package rgb_lib
 
 /*
-#cgo LDFLAGS: -lrgblibuniffi -L${SRCDIR}/lib -rpath=\$ORIGIN/lib
+#cgo LDFLAGS: -lrgblibuniffi -L${SRCDIR}/lib -Wl,-rpath=\$ORIGIN/lib
 */
 // #include <rgb_lib.h>
 import "C"
